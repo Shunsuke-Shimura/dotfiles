1555ca681e7a09a9c02c4b922f259bcf9e9f6659
Shunsuke Shimura <7319069@ed.tus.ac.jp>
2020-12-22 13:50:51 +0900

    added shellscript file for linux to make simbolic link to HOME directory

---
+++ makeLinkToHome.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ln -s ~/config_files/.vimrc ~/.vimrc
+echo Created simbolic file of .vimrc to HOME dir
+ln -s ~/config_files/.gvimrc ~/.gvimrc
+echo Created sinbolic file of .gvimrc to HOME dir
